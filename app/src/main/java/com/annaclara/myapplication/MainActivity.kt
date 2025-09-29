d2b299b408dcb2aaeddf40301bff84d056cb7522
20231tiimi0375 <annasan.vieira@gmail.com>
2025-09-29 11:31:54 -0300

    falta estetica

--- app/src/main/java/com/annaclara/myapplication/MainActivity.kt
+++ app/src/main/java/com/annaclara/myapplication/MainActivity.kt
@@ -5,10 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.MotionEvent
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val imgProfile: ImageView = findViewById(R.id.imgProfile)
+
+        imgProfile.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    v.animate().scaleX(1.1f).scaleY(1.1f).setDuration(150).start()
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    v.animate().scaleX(1f).scaleY(1f).setDuration(150).start()
+                }
+            }
+            true
+        }
     }
 }
+
+
+
---
+++ app/src/main/java/com/annaclara/myapplication/ProfileActivity.kt
@@ -0,0 +1,7 @@
+package com.annaclara.myapplication
+
+import android.app.Activity
+
+class ProfileActivity : Activity() {
+
+}
